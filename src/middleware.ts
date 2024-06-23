3ba4cefabc3a1ecc3a112f34993a0350dfa2c0a6
cristhiankelm <criskp007@gmail.com>
2024-06-23 14:58:50 -0400

    fix redirect / route

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,15 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token');
 
+  // Redirecionar a rota '/' para o dashboard ou login
+  if (request.nextUrl.pathname === '/') {
+    if (token) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
+    } else {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+
   // Se não houver token e a rota não for login, redireciona para login
   if (!token && request.nextUrl.pathname !== '/login') {
     return NextResponse.redirect(new URL('/login', request.url));
@@ -18,5 +27,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/clients/:path*', '/login'], // Defina as rotas que você deseja proteger
+  matcher: ['/', '/dashboard/:path*', '/clients/:path*', '/login'], // Defina as rotas que você deseja proteger
 };
